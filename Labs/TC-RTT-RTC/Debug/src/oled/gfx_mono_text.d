4e4ce335ebe50769cf364ec860e8973a8e9066da
gabrielzezze <gabrielzezze@me.com>
2020-04-23 14:24:15 -0300

    Lab7 RTOS7 Lab7 RTOS-LCD B Done

--- Labs/TC-RTT-RTC/Debug/src/oled/gfx_mono_text.d
+++ Labs/TC-RTT-RTC/Debug/src/oled/gfx_mono_text.d
@@ -165,9 +165,13 @@ src/oled/gfx_mono_text.d src/oled/gfx_mono_text.o: \
  ../src/ASF/sam/drivers/rtc/rtc.h ../src/ASF/sam/drivers/rtt/rtt.h \
  ../src/ASF/sam/utils/fpu/fpu.h \
  ../src/ASF/sam/boards/same70_xplained/led.h \
- ../src/ASF/sam/drivers/spi/spi.h ../src/ASF/sam/drivers/tc/tc.h \
- ../src/ASF/sam/drivers/pio/pio_handler.h ../src/config/conf_board.h \
- ../src/oled/gfx_mono_framebuffer.h
+ ../src/ASF/sam/drivers/spi/spi.h \
+ ../src/ASF/common/services/sleepmgr/sam/sleepmgr.h \
+ ../src/config/conf_sleepmgr.h \
+ ../src/ASF/common/services/sleepmgr/sleepmgr.h \
+ ../src/ASF/common/services/sleepmgr/sam/sleepmgr.h \
+ ../src/ASF/sam/drivers/tc/tc.h ../src/ASF/sam/drivers/pio/pio_handler.h \
+ ../src/config/conf_board.h ../src/oled/gfx_mono_framebuffer.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h:
 
@@ -516,6 +520,14 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-no
 ../src/ASF/sam/boards/same70_xplained/led.h:
 
 ../src/ASF/sam/drivers/spi/spi.h:
+
+../src/ASF/common/services/sleepmgr/sam/sleepmgr.h:
+
+../src/config/conf_sleepmgr.h:
+
+../src/ASF/common/services/sleepmgr/sleepmgr.h:
+
+../src/ASF/common/services/sleepmgr/sam/sleepmgr.h:
 
 ../src/ASF/sam/drivers/tc/tc.h:
 
--- Labs/TC-RTT-RTC/src/main.c
+++ Labs/TC-RTT-RTC/src/main.c
@@ -215,8 +215,8 @@ int main (void)
 	gfx_mono_ssd1306_init();
   
 	// Escreve na tela um circulo e um texto
-	//gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
-	//gfx_mono_draw_string("mundo", 50,16, &sysfont);
+	gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
+	gfx_mono_draw_string("mundo", 50,16, &sysfont);
 
   /* Insert application code here, after the board has been initialized. */
 	while(1) {
